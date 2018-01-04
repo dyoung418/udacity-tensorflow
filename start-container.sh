bab8b23c4c602f42bf7367ca2be56f1e9c18293a
Danny Young <git@youngshome.com>
2018-01-04 00:30:43 -0800

    small changes after installing linux mint on danny-blade -- new tensorflow for nvidia calls

--- start-container.sh
+++ start-container.sh
@@ -4,3 +4,6 @@
 
 #docker run -it -p 8888:8888 -p 8081:8081 --name udacity-tf --rm -v ~/udacity-tensorflow:/notebooks b.gcr.io/tensorflow-udacity/assignments:0.5.0
 docker run -it -p 8888:8888 -p 8081:8081 --name udacity-tf --rm -v ~/udacity-tensorflow:/notebooks b.gcr.io/tensorflow-udacity/assignments:latest
+
+# for nvidia:
+#docker run --runtime=nvidia -it -p 8888:8888 -p 8081:8081 --name udacity-tf --rm -v ~/udacity-tensorflow:/notebooks gcr.io/tensorflow/tensorflow:latest-gpu
